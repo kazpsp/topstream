887e6da382552ccb28e04a2205927f32be8827a9
kazpsp <rodrigozurek@gmail.com>
2020-04-12 13:13:00 -0500

    Auth fix

--- db/migrate/20200411232729_addjtiusers.rb
+++ db/migrate/20200411232729_addjtiusers.rb
@@ -1,6 +1,6 @@
 class Addjtiusers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :jti, :string, null: false
+    add_column :users, :jti, :string, null: false, default: "error"
     add_index :users, :jti, unique: true
   end
 end
